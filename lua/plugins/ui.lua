f7a069cd7be24f4d466a2dae49aaa2357fc03960
Asad Moosvi <moosvi.asad@gmail.com>
2025-04-09 16:10:02 +0500

    feat(ui): set minimal bufferline theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,17 @@
 return {
   -- colorscheme
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- See https://github.com/folke/tokyonight.nvim/issues/703#issuecomment-2765214186
+      on_highlights = function(hl, c)
+        hl.TabLineFill = {
+          bg = c.none,
+        }
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
@@ -20,5 +31,15 @@ return {
         },
       },
     },
+  },
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    opts = function(_, opts)
+      local bufferline = require("bufferline")
+      opts.options.style_preset = bufferline.style_preset.minimal
+      opts.options.show_buffer_close_icons = false
+      opts.options.show_close_icon = false
+    end,
   },
 }

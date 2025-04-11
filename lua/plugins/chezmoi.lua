c002186c22e76b931466a91398ea46980d28d373
Asad Moosvi <moosvi.asad@gmail.com>
2025-04-11 19:10:39 +0500

    Revert "Remove chezmoi extra"
    This reverts commit 03dc27c4de9e9c0187ed45fb339b767c28cdfd71.
    
    Add chezmoi extra back in. Include a check so that scripts within
    `.chezmoiscripts` do not automatically get applied.

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -1,7 +1,25 @@
 return {
-  -- highlighting for chezmoi files template files
-  "alker0/chezmoi.vim",
-  init = function()
-    vim.g["chezmoi#use_tmp_buffer"] = 1
-  end,
+  {
+    -- highlighting for chezmoi files template files
+    "alker0/chezmoi.vim",
+    init = function()
+      vim.g["chezmoi#use_tmp_buffer"] = 1
+    end,
+  },
+  {
+    "xvzc/chezmoi.nvim",
+    init = function()
+      -- run chezmoi edit on file enter
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function()
+          local filepath = vim.fn.expand("%:p")
+          -- dont apply scripts
+          if not filepath:match("/.chezmoiscripts/") then
+            vim.schedule(require("chezmoi.commands.__edit").watch)
+          end
+        end,
+      })
+    end,
+  },
 }

37c07ad46db3bbf18ee4b4728aa451d20b4496ba
Asad Moosvi <moosvi.asad@gmail.com>
2025-07-11 12:01:47 +0500

    feat(treesitter): configure textobject parameter swapping

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,5 +2,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     auto_install = true,
+    textobjects = {
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<M-l>"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<M-h>"] = "@parameter.inner",
+        },
+      },
+    },
   },
 }

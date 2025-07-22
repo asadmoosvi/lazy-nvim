faa22573473cf36ff1701349ac5f62b1a6ebcae7
Asad Moosvi <moosvi.asad@gmail.com>
2025-07-22 10:35:13 +0500

    feat(autocmds): add keymap to compile and run C++ files
    Introduces a keymap for C++ files to compile and run them in a terminal.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,39 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "css",
   command = "setlocal formatoptions-=cro",
 })
+
+-- run cpp files using <leader>r
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.keymap.set("n", "<leader>r", function()
+      -- find and close the previous terminal buffer
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[buf].buftype == "terminal" and vim.b[buf].cpp_runner then
+          vim.api.nvim_buf_delete(buf, { force = true })
+          break
+        end
+      end
+
+      -- compile and run the c++ file
+      vim.cmd("w") -- Save the current file
+      local file = vim.fn.expand("%:p")
+      local output_file = vim.fn.expand("%:p:r")
+      local cmd = "g++ " .. file .. " -o " .. output_file .. " && " .. output_file
+
+      -- open the terminal in a split
+      if vim.o.columns > 120 then
+        vim.cmd("vertical terminal " .. cmd)
+      else
+        vim.cmd("horizontal terminal " .. cmd)
+      end
+
+      -- mark the new terminal buffer
+      local term_buf = vim.api.nvim_get_current_buf()
+      vim.b[term_buf].cpp_runner = true
+
+      -- return focus to the previous window
+      vim.cmd("wincmd p")
+    end, { desc = "Run C++ File", buffer = true })
+  end,
+})

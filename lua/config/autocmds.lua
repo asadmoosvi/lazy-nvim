23c9d1feae4e5fa1f0405da9c0400c0d79f113b5
Asad Moosvi <moosvi.asad@gmail.com>
2025-07-23 10:50:09 +0500

    feat(autocmds): add generic code runner function
    The runner is currently available for C++ and Python via the `<leader>r`
    keymap.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,38 +21,61 @@ vim.api.nvim_create_autocmd("TermClose", {
   end,
 })
 
+-- Generic function to set up a code runner keymap
+local function setup_code_runner(opts)
+  vim.keymap.set("n", "<leader>r", function()
+    -- find and close the previous terminal buffer
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf].buftype == "terminal" and vim.b[buf][opts.runner_id] then
+        vim.api.nvim_buf_delete(buf, { force = true })
+        break
+      end
+    end
+
+    -- prepare the command
+    vim.cmd("w") -- Save the current file
+    local file = vim.fn.expand("%:p")
+    local output_file = vim.fn.expand("%:p:r")
+    local cmd = opts.command
+    cmd = cmd:gsub("%%file%%", vim.fn.fnameescape(file))
+    cmd = cmd:gsub("%%output%%", vim.fn.fnameescape(output_file))
+
+    -- open the terminal in a split
+    if vim.o.columns > 120 then
+      vim.cmd("vertical terminal " .. cmd)
+    else
+      vim.cmd("15split | terminal " .. cmd)
+    end
+
+    -- mark the new terminal buffer
+    local term_buf = vim.api.nvim_get_current_buf()
+    vim.b[term_buf][opts.runner_id] = true
+
+    -- return focus to the previous window
+    vim.cmd("wincmd p")
+  end, { desc = opts.desc, buffer = true })
+end
+
 -- run cpp files using <leader>r
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
-    vim.keymap.set("n", "<leader>r", function()
-      -- find and close the previous terminal buffer
-      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.bo[buf].buftype == "terminal" and vim.b[buf].cpp_runner then
-          vim.api.nvim_buf_delete(buf, { force = true })
-          break
-        end
-      end
+    setup_code_runner({
+      runner_id = "cpp_runner",
+      command = "g++ %file% -o %output% && %output%",
+      desc = "Run C++ File",
+    })
+  end,
+})
 
-      -- compile and run the c++ file
-      vim.cmd("w") -- Save the current file
-      local file = vim.fn.expand("%:p")
-      local output_file = vim.fn.expand("%:p:r")
-      local cmd = "g++ " .. file .. " -o " .. output_file .. " && " .. output_file
-
-      -- open the terminal in a split
-      if vim.o.columns > 120 then
-        vim.cmd("vertical terminal " .. cmd)
-      else
-        vim.cmd("horizontal terminal " .. cmd)
-      end
-
-      -- mark the new terminal buffer
-      local term_buf = vim.api.nvim_get_current_buf()
-      vim.b[term_buf].cpp_runner = true
-
-      -- return focus to the previous window
-      vim.cmd("wincmd p")
-    end, { desc = "Run C++ File", buffer = true })
+-- run python files using <leader>r
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    setup_code_runner({
+      runner_id = "python_runner",
+      command = "python %file%",
+      desc = "Run Python File",
+    })
   end,
 })

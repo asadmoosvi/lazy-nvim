8ee501c8aeed133d02af305bd529c1d09033758e
Asad Moosvi <moosvi.asad@gmail.com>
2026-06-03 16:54:05 +0500

    fix(treesitter): restore textobject swap mappings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    auto_install = true,
-    textobjects = {
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<M-l>"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<M-h>"] = "@parameter.inner",
-        },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "htmldjango",
       },
     },
-    ensure_installed = {
-      "htmldjango",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    keys = {
+      {
+        "<M-l>",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+        end,
+        mode = "n",
+        desc = "Swap Next Parameter",
+      },
+      {
+        "<M-h>",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
+        end,
+        mode = "n",
+        desc = "Swap Previous Parameter",
+      },
     },
   },
 }

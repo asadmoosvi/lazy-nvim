97fc55f4154500937219a05cc1260c2b47b638d9
Asad Moosvi <moosvi.asad@gmail.com>
2026-06-20 22:59:38 +0500

    feat(conform): use djlint as the formatter for htmldjango files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,15 +15,25 @@ return {
     },
   },
   {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "djlint" })
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
         python = { "ruff_fix", "ruff_format" },
+        htmldjango = { "djlint" },
       },
       formatters = {
         ruff_fix = {
           -- sort imports: ruff check --fix --select I
           append_args = { "--select", "I" },
+        },
+        djlint = {
+          append_args = { "--indent", 2 },
         },
       },
     },

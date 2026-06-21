7aaa8247dd49de9fb83b1ee31fb016883d0fde6e
Asad Moosvi <moosvi.asad@gmail.com>
2026-06-21 17:50:53 +0500

    Remove treesitter htmldjango parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      auto_install = true,
-      ensure_installed = {
-        "htmldjango",
-      },
       indent = {
         disable = { "go" },
       },
